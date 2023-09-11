4e428c949b3da3c56d0c740fd8cc3f4b46d68fd3
Chirstian Camilo Barrera Salinas <camilobr89@hotmail.com>
2023-09-11 11:34:00 -0500

    se agrega comentario

--- challengeThree.js
+++ challengeThree.js
@@ -1,6 +1,6 @@
 function challengeThree(arr) {
     arr.sort((a, b) => a - b);
-    let minChange = 1;
+    let minChange = 3;
 
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] <= minChange) {
@@ -13,8 +13,7 @@ function challengeThree(arr) {
     return minChange;
 }
 
-console.log(challengeThree([1,2,4,7]));
-
+console.log(challengeThree([1, 2, 3, 5, 6, 8, 9, 10, 14, 16])); // prueba de Acero Garcia Fabian 
 console.log(challengeThree([5, 7, 1, 1, 2, 3, 22]));
 console.log(challengeThree([1, 1, 1, 1, 1]));
 console.log(challengeThree([1, 5, 1, 1, 1, 10, 15, 20, 100]));
